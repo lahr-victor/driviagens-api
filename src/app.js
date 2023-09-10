8f46405eae163dee49d41b8ab1f87860294668a5
Victor Lahr <lahr.victor@gmail.com>
2023-09-10 12:34:47 -0300

    build: install basic packages and create initial configuration files

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import chalk from 'chalk';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import 'express-async-errors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(chalk.bgGreen(`Running server on port ${port}! 🚀`));
+});
