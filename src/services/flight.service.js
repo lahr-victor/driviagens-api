e8076232abda9e49551cf7e3f396b52f66102ff1
Victor Lahr <lahr.victor@gmail.com>
2023-09-11 17:37:27 -0300

    fix: format flight date properly to work on deployed server and database

--- src/services/flight.service.js
+++ src/services/flight.service.js
@@ -1,7 +1,11 @@
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat.js';
 import cityRepository from '../repositories/city.repository.js';
 import flightRepository from '../repositories/flight.repository.js';
 import notFoundError from '../errors/notFound.error.js';
 import incompatibleError from '../errors/incompatible.error.js';
+
+dayjs.extend(customParseFormat);
 
 async function create(origin, destination, date) {
   const existingOrigin = await cityRepository.validateById(origin);
@@ -12,7 +16,9 @@ async function create(origin, destination, date) {
 
   if (origin === destination) throw incompatibleError('Flights');
 
-  return flightRepository.create(origin, destination, date);
+  const formattedDate = dayjs(date, 'DD-MM-YYYY').toISOString();
+
+  return flightRepository.create(origin, destination, formattedDate);
 }
 
 const flightService = { create };
